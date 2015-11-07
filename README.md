4e308455fab2da049815841a2bf6052b1b6a13a6
Jiaqi Gu <fudanvictor@gmail.com>
2015-11-06 23:58:52 -0800

    add demo

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 + Only Two JavaScript Files, one for data and one for engine, no other dependencies needed
 + Data file easy to be read/modified by non-technicals
 + Template organized by sections
++ See data.js and demo.html for usage
---
+++ data.js
@@ -0,0 +1,31 @@
+var data = function () {/*
+###
+### comments starts with #, empty lines are also ignored
+### each section starts with [template:name]
+### contents of lines in each group within a section replace variables
+### {0}, {1}, {2}, {3}, ... in html file
+###
+
+[template:events]
+
+1.2
+event1
+http://example1.com
+
+5.28
+event2
+http://example2.com
+
+10.15
+event3
+http://example3.com
+
+[template:story]
+
+故事1
+http://example1.com
+
+故事2
+http://example2.com
+
+*/};
